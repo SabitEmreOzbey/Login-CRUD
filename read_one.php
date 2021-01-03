453472f7b814e62ea0f52af65970a32bc3792144
Emre Özbey <50449797+SabitEmreOzbey@users.noreply.github.com>
2021-01-03 19:02:42 +0300

    ImageBugisSolved

--- read_one.php
+++ read_one.php
@@ -64,7 +64,7 @@ echo "<table class='table table-hover table-responsive table-bordered'>";
     echo "<tr>";
     echo "<td>Image</td>";
     echo "<td>";
-        echo $product->image ? "<img src='uploads/{$product->image}' style='width:300px;' />" : "No image found.";
+        echo $product->image ? "<img src='admin/uploads/{$product->image}' style='width:300px;' />" : "No image found.";
     echo "</td>";
 echo "</tr>";
  
